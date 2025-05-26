77dcc014fc61cbfb4ec60be14c5e2dde4c84257c
rafael1209 <mr.chasmanrafael@gmail.com>
2025-05-26 20:08:04 +0300

    Add IPaymentService interface with method signatures
    Introduced a new interface `IPaymentService` in the
    `CaseBattleBackend.Interfaces` namespace. This interface
    defines two asynchronous methods: `CreateInvoice()` and
    `ProcessPayment()`, with TODO comments indicating that
    implementation is pending.

---
+++ Interfaces/IPaymentService.cs
@@ -0,0 +1,7 @@
+﻿namespace CaseBattleBackend.Interfaces;
+
+public interface IPaymentService
+{
+    Task CreateInvoice(); //TODO: realize this shit
+    Task ProcessPayment(); //TODO: realize this shit
+}
