f6bb2dd0cf1eac08f0bc2e2ca7c222399688addc
rafael1209 <mr.chasmanrafael@gmail.com>
2025-05-26 19:48:17 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CaseBattleBackend;
+
+public class Startup(IConfiguration configuration)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CaseBattle API", Version = "v1" });
+            c.AddSecurityDefinition(
+                "ApiKey",
+                new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Header,
+                    Description =
+                        "API Key needed to access the endpoints. API Key must be in the 'Authorization' header. For public endpoints you can didn't use API Key, or just type '0'",
+                    Name = "Authorization"
+                }
+            );
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                { new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "ApiKey" } }, [] }
+            });
+        });
+
+        services.AddHttpContextAccessor();
+        services.AddMemoryCache();
+
+        services.AddSingleton<IMongoDbContext, MongoDbContext>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddKeyedScoped<IOAuthProvider, GoogleAuthService>(AuthStrategies.Google);
+        services.AddKeyedScoped<IOAuthProvider, DiscordAuthService>(AuthStrategies.Discord);
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ITokenService, TokenService>();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyMethod()
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader();
+            });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHsts();
+        }
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseCors("AllowAll");
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute().AllowAnonymous();
+            endpoints.MapSwagger();
+            endpoints.MapControllers().AllowAnonymous();
+        });
+    }
+}
