7886cbd654d4fa2c0a7bc19099260ac237265336
rafael1209 <mr.chasmanrafael@gmail.com>
2025-05-27 18:48:14 +0300

    Add UsersController and UserInfo model for user data
    Introduced the `UsersController` to manage user-related API requests, including a `GetMe` method for retrieving the current user's information with authentication. Added the `UserInfo` class to encapsulate user details such as ID, balance, nickname, Minecraft UUID, and level.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using CaseBattleBackend.Interfaces;
+using CaseBattleBackend.Middlewares;
+using CaseBattleBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseBattleBackend.Controllers;
+
+[Route("api/v1/users")]
+public class UsersController(IUserService userService) : Controller
+{
+    [HttpGet("@me")]
+    [AuthMiddleware]
+    public async Task<IActionResult> GetMe()
+    {
+        var user = HttpContext.Items["@me"] as User;
+
+        var userInfo = new UserInfo(user.Id.ToString(), user.Balance, user.Username, user.MinecraftUuid, 0);
+
+        return Ok(userInfo);
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace CaseBattleBackend.Models;
+
+public class UserInfo(string id, double balance, string nickname, string uuid, int level)
+{
+    public string Id { get; set; } = id;
+    public double Balance { get; set; } = balance;
+    public string Nickname { get; set; } = nickname;
+    public string MinecraftUuid { get; set; } = uuid;
+    public int Level { get; set; } = level;
+}
