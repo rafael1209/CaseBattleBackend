975781dbb9ec0b084311f5b3f4e838497c0a3f60
rafael1209 <mr.chasmanrafael@gmail.com>
2025-08-01 15:00:37 +0300

    Refactor CasesController constructor and imports
    Removed unused import for Microsoft.IdentityModel.Tokens.
    Updated the constructor syntax to directly accept an ICaseService parameter.

--- Controllers/CasesController.cs
+++ Controllers/CasesController.cs
@@ -4,7 +4,6 @@ using CaseBattleBackend.Middlewares;
 using CaseBattleBackend.Models;
 using CaseBattleBackend.Requests;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace CaseBattleBackend.Controllers;
 
