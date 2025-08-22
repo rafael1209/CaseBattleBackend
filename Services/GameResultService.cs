1221eaa9d3217249b2056b21d30d3f35d434f164
rafael1209 <mr.chasmanrafael@gmail.com>
2025-08-22 18:25:33 +0300

    Enhance GameResultService with user and case data
    Updated GameResultService to include IUserService and ICaseService dependencies.
    Modified SaveResult method to retrieve user and case data, ensuring both are valid before processing.
    Expanded LiveWin object creation to include user and case details.
    Refactored ImageUrl property for improved readability while maintaining logic.

--- Services/GameResultService.cs
+++ Services/GameResultService.cs
@@ -11,6 +11,8 @@ public class GameResultService(
     IMinecraftAssets minecraftAssets,
     IStorageService storageService,
     IItemRepository itemRepository,
+    IUserService userService,
+    ICaseService caseService,
     WebSocketServerService webSocketServer) : IGameResult
 {
     public async Task SaveResult(User user, Case caseData, CaseItemView item, ObjectId gameId)
@@ -55,12 +57,28 @@ public class GameResultService(
         foreach (var game in gameResults)
         {
             var item = await itemRepository.GetById(game.ItemId);
+            var user = await userService.GetUserInfo(game.UserId.ToString());
+            var caseData = await caseService.GetById(game.CaseId.ToString());
 
-            if (item is null)
+            if (item is null || caseData is null)
                 continue;
 
             liveWins.Add(new LiveWin
             {
+                User = new UserDto
+                {
+                    Id = user.Id,
+                    Username = user.Nickname,
+                    AvatarUrl = user.AvatarUrl
+                },
+                Case = new CaseDto
+                {
+                    Id = caseData.Id,
+                    Name = caseData.Name,
+                    Description = caseData.Description,
+                    ImageUrl = caseData.ImageUrl,
+                    Price = caseData.Price
+                },
                 Item = new CaseItemView
                 {
                     Id = game.ItemId.ToString(),
